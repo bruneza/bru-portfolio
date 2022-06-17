7f592a9c9678883abb505da51998e8c852154c73
unknown <am.bruneza@gmail.com>
2022-06-18 00:18:05 +0200

    portfolio banner initialized

--- src/Comps/banner-section/banner.js
+++ src/Comps/banner-section/banner.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './banner.css'
 
 function banner() {
   return (
     <div className='banner-row'>
-        <div className="banner-col-1">col 1</div>
+        <div className="banner-col-1">
+            <div className="wrapper">
+                <span className="bru-subtitle">Hello,</span>
+                <h1>my Name is Bruce Mugwaneza</h1>
+                <div className="banner-pos">
+                    <div className="banner-pos-items">
+                    <span>I'am </span>
+                        <ul className="banner-pos-item-lis">
+                            <li className="banner-pos-item">Web Developer</li>
+                            <li className="banner-pos-item">Graphic Designer</li>
+                            <li className="banner-pos-item">Social Media Manager</li>
+                        </ul>
+                    </div>
+                </div>    
+            </div>
+        </div>
         <div className="banner-col-2">col 2</div>
     </div>
   )
